3a5bc4201c4bd0ac5abf9e8d1c0d7d0203892d4b
Rajesh <80874249+RajeshHsejar27@users.noreply.github.com>
2023-04-05 15:40:35 +0530

    modified user cart sidebar

--- client/src/components/cards/UserCartSidebar.js
+++ client/src/components/cards/UserCartSidebar.js
@@ -105,15 +105,16 @@ export default function UserCartSidebar(){
                              
                                     <div className="mt-3">
                                     {!auth?.user || !cart?.length ? '': <>
-                                    <h4 style={{color:"white"}}>Tap the below button to finish the trade!</h4><hr  style={{color:"white"}}></hr>
-                                    <p style={{color:"white",fontSize:"15px"}}>Make sure to check the availability of the artifact in stock and compare it to your selected count 
+                                    <h4 style={{color:"white"}}>Update the address before proceeding to trade!</h4><hr  style={{color:"white"}}></hr>
+                                    <p style={{color:"white",fontSize:"15px"}}>Compare the availability of the artifact in stock and selected count 
                                      before proceeding payment. Since the stock isn't updated until a purchase has been made,
                                    you can add infinite items. But don't be stupid like that since you will get only the available stock 
                                    and a refund for the remaining quantity.</p>
-                                    <h3 style={{color:"white"}}>Tap the below button to finish the trade!</h3>
+                                    <h4 style={{color:"white"}}>Tap the below button to finish the trade!</h4>
                                     <button className="btn btn-outline-light mt-3"
                                         onClick={handleShow}
-                                        >Let's trade!</button>  
+                                        disabled={!auth?.user?.address || loading}
+                                        >{loading ? "Processing..." : "Let's trade!"}</button>  
 
                                     <Modal
                                             show={show}
@@ -126,13 +127,20 @@ export default function UserCartSidebar(){
                                             </Modal.Header>
                                             <Modal.Body>
                                            <img src={QR} style={{display:"block",marginLeft:"auto",marginRight:"auto",width:"50%"}} alt="qr code" /><hr></hr>
-                                           <h5 style={{color:"black"}}>Trading instructions:</h5>
-                                           <h6 style={{color:"black"}}>Scan the QR code from your GPAY to finish the trade and make sure to enter the
-                                           correct amount of the purchase which is clearly shown in the cart total. After the successful payment, YOU MUST CLICK 
+                                           <h5 style={{color:"black"}}> <u> <b> Trading instructions: </b> </u> </h5>
+                                           <h6 style={{color:"black"}}>
+                                            <ul>
+                                            <li> Scan the QR code from your GPAY to finish the trade and make sure to enter the
+                                           correct amount of the purchase which is clearly shown in the cart total.</li>
+                                           <li>After the successful payment, YOU MUST CLICK 
                                            THE "TRADE DONE!" button so that it will invoke some functions and the Merchant can update the stocks and also update your order and post the 
-                                           status of your order through mail. Your order will be processed,shipped and delivered through the Wagon of Mayhem.
+                                           status of your order through mail (Given that the email is correct).</li> 
+                                           <li>Your order will be processed,shipped and delivered through the Wagon of Mayhem.</li> 
+                                            </ul>
                                            <hr></hr>
-                                            Incase you want to pay later and not now, click the "Will trade by later!" button.
+                                            Incase if you want to pay later and just stalking, click the "Will trade by later!" button.
+                                            <hr></hr>
+                                            If you give "Trade done!" when u actually didnt and mess up with my program logic,believe me,I will know,I will find u and kick ur ass.Thank you,have a nice day!
                                             </h6>
                                             </Modal.Body>
                                             <Modal.Footer>
