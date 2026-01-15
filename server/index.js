e7465bca21f7ee83d7e86a5724679c20b902f367
Rajesh N <80874249+RajeshHsejar27@users.noreply.github.com>
2026-01-15 15:56:32 +0530

    Add ping route for health check
    Added a simple ping/health route for frontend checks.

--- server/index.js
+++ server/index.js
@@ -3,22 +3,20 @@ import express from 'express';      //used as server
 import dotenv from 'dotenv';        //Loads environment variables from .env file
 import mongoose from 'mongoose';    //used to model the data and manage the DB easily
 import authRoutes from '../server/routes/auth.js'; 
-            //generate all the routes requires fr user authentication
 import morgan from 'morgan';        //HTTP request logger middleware
-import  categoryRoutes from './routes/category.js'        //HTTP request logger middleware
+import categoryRoutes from './routes/category.js';
 import productRoutes from './routes/product.js';
 import cors from 'cors'; //a cross origin resource sharing library
 
 dotenv.config();
 
-const app=express(); 
-
+const app = express(); 
 
 //database connection
 mongoose
-        .connect(process.env.M_URI)
-        .then(()=>console.log("db connected"))
-        .catch((err)=>console.log("error : ",err));
+  .connect(process.env.M_URI)
+  .then(() => console.log("db connected"))
+  .catch((err) => console.log("error : ", err));
 
 //middlewares
 app.use(cors());
@@ -26,16 +24,19 @@ app.use(morgan("dev"));
 app.use(express.json());
 
 //router middleware
-app.use("/api",authRoutes);
-app.use("/api",categoryRoutes);
-app.use("/api",productRoutes);
+app.use("/api", authRoutes);
+app.use("/api", categoryRoutes);
+app.use("/api", productRoutes);
 
-
-//getting port value from .env file
-const port=process.env.PORT || 8000;
-
-//logging server initiation
-app.listen(port,()=>{   
-    console.log(`node server started on port ${port}`)
+//  Adding a simple ping/health route to run when frontend loads
+app.get("/ping", (req, res) => {
+  res.send("pong");   // lightweight response
 });
 
+//getting port value from .env file
+const port = process.env.PORT || 8000;
+
+//logging server initiation
+app.listen(port, () => {   
+  console.log(`node server started on port ${port}`)
+});
